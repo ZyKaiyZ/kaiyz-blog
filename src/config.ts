ab60e2ed8b4447ba9bba330d373f7bca076a0eb0
ZyKaiyZ <yoyo8117431@gmail.com>
2023-12-28 18:11:39 +0800

    Update base route

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ export const SITE: Site = {
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 5,
-  base: "/",
+  base: "",
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
